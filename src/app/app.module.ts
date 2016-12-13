2f777d75651f89f87acca018af3eaf456f512722
hugo <mbiz.hugo@gmail.com>
2016-12-13 17:06:31 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+// core
+import { NgModule, enableProdMode }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+// app
+enableProdMode();
+// animate
+import {
+  Component,
+  Input,
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/core';
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  imports:      [ 
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule 
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ 
+    AppComponent, AppComponent
+    ],
+    providers: [
+      ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
